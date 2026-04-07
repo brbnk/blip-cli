cde4a543743297cd8c4537f0ecb1dd84b769c945
brbnk <bruno.nakayabu@gmail.com>
2026-04-07 10:28:16 -0300

    refact: impl ContextStore functions

--- blip-cli/cli/src/types/cmds/analyze.rs
+++ blip-cli/cli/src/types/cmds/analyze.rs
@@ -40,7 +40,7 @@ impl Runnable for Analyze {
                 &self.commong_args.tenant, &self.commong_args.bot);
                 
             for child in router_children {
-                let mut context_store = create_context_store(&child.short_name);
+                let mut context_store = ContextStore::new(&child.short_name);
 
                 let bot_flow = get_bot_flow(&self.commong_args.tenant, &child.short_name);
                 
@@ -48,31 +48,31 @@ impl Runnable for Analyze {
                     Ok(json) => {
                         for (_, state) in json.flow {
                             context_store.states += 1;
-                            update_store(state, &mut context_store);
+                            context_store.update(state);
                         }
                     },
                     Err(_) => {},
                 }
 
-                print_result(context_store);
+                context_store.print_result();
             }
         }
         else {
             let bot_flow = get_bot_flow(&self.commong_args.tenant, &self.commong_args.bot);
             
-            let mut context_store = create_context_store(&self.commong_args.bot);
+            let mut context_store = ContextStore::new(&self.commong_args.bot);
             
             match bot_flow {
                 Ok(json) => {
                     for (_, state) in json.flow {
                         context_store.states += 1;
-                        update_store(state, &mut context_store);
+                        context_store.update(state);
                     }
                 }
                 Err(_) => {},
             }
             
-            print_result(context_store);
+            context_store.print_result();
         }
     }
 }
@@ -93,67 +93,70 @@ struct ContextStore {
     desk: i32
 }
 
-fn create_context_store(bot_id: &str) -> ContextStore {
-    ContextStore { 
-        bot_id: String::from(bot_id),
-        states: 0,
-        scripts_v1: 0,
-        scripts_v2: 0,
-        http: 0,
-        commands: 0,
-        trackings: 0,
-        variables: 0,
-        redirects: 0,
-        blip_function: 0,
-        merge_contacts: 0,
-        agents: 0,
-        desk: 0,
+impl ContextStore {
+    pub fn new(bot_id: &str) -> Self {
+        ContextStore { 
+            bot_id: String::from(bot_id),
+            states: 0,
+            scripts_v1: 0,
+            scripts_v2: 0,
+            http: 0,
+            commands: 0,
+            trackings: 0,
+            variables: 0,
+            redirects: 0,
+            blip_function: 0,
+            merge_contacts: 0,
+            agents: 0,
+            desk: 0,
+        }
+    }
+
+    pub fn print_result(&self) {
+        print_state_title(&self.bot_id);
+        println(&format!("States = {}", self.states), Color::White);
+        println(&format!("ScriptsV1 = {}", self.scripts_v1), Color::White);
+        println(&format!("ScriptsV2 = {}", self.scripts_v2), Color::White);
+        println(&format!("ProcessHttp = {}", self.http), Color::White);
+        println(&format!("ProcessCommands = {}", self.commands), Color::White);
+        println(&format!("Trackings = {}", self.trackings), Color::White);
+        println(&format!("Variables = {}", self.variables), Color::White);
+        println(&format!("Redirects = {}", self.redirects), Color::White);
+        println(&format!("BlipFunctions = {}", self.blip_function), Color::White);
+        println(&format!("MergeContacts = {}", self.merge_contacts), Color::White);
+        println(&format!("Agents = {}", self.agents), Color::White);
+        println(&format!("Desk = {}", self.desk), Color::White);
+        println!();
+    }
+    
+    pub fn update(&mut self, state: State) {
+        for eca in state.entering_custom_actions {
+            self.increment(eca.action_type);
+        }
+    
+        for lca in state.leaving_custom_actions {
+            self.increment(lca.action_type);
+        }
+    }
+    
+    fn increment(&mut self, action_type: ActionType) {
+        match action_type {
+            ActionType::ExecuteScript => self.scripts_v1 += 1,
+            ActionType::SetVariable => self.variables += 1,
+            ActionType::ProcessHttp => self.http += 1,
+            ActionType::MergeContact => self.merge_contacts += 1,
+            ActionType::Redirect => self.redirects += 1,
+            ActionType::ExecuteScriptV2 => self.scripts_v2 += 1,
+            ActionType::ProcessCommand => self.commands += 1,
+            ActionType::ExecuteBlipFunction => self.blip_function += 1,
+            ActionType::ProcessContentAssistant => {},
+            ActionType::TrackEvent => self.trackings += 1,
+            ActionType::ForwardToAgent => self.agents += 1,
+            ActionType::ForwardToDesk => self.desk += 1,
+        }
     }
 }
 
-fn print_result(context: ContextStore) {
-    print_state_title(&context.bot_id);
-    println(&format!("States = {}", context.states), Color::White);
-    println(&format!("ScriptsV1 = {}", context.scripts_v1), Color::White);
-    println(&format!("ScriptsV2 = {}", context.scripts_v2), Color::White);
-    println(&format!("ProcessHttp = {}", context.http), Color::White);
-    println(&format!("ProcessCommands = {}", context.commands), Color::White);
-    println(&format!("Trackings = {}", context.trackings), Color::White);
-    println(&format!("Variables = {}", context.variables), Color::White);
-    println(&format!("Redirects = {}", context.redirects), Color::White);
-    println(&format!("BlipFunctions = {}", context.blip_function), Color::White);
-    println(&format!("MergeContacts = {}", context.merge_contacts), Color::White);
-    println(&format!("Agents = {}", context.agents), Color::White);
-    println(&format!("Desk = {}", context.desk), Color::White);
-    println!();
-}
-
-fn update_store(state: State, context_store: &mut ContextStore) {
-    for eca in state.entering_custom_actions {
-        increment_counter(eca.action_type, context_store);
-    }
-
-    for lca in state.leaving_custom_actions {
-        increment_counter(lca.action_type, context_store);
-    }
-}
-
-fn increment_counter(action_type: ActionType, context_store: &mut ContextStore) {
-    match action_type {
-        ActionType::ExecuteScript => context_store.scripts_v1 += 1,
-        ActionType::SetVariable => context_store.variables += 1,
-        ActionType::ProcessHttp => context_store.http += 1,
-        ActionType::MergeContact => context_store.merge_contacts += 1,
-        ActionType::Redirect => context_store.redirects += 1,
-        ActionType::ExecuteScriptV2 => context_store.scripts_v2 += 1,
-        ActionType::ProcessCommand => context_store.commands += 1,
-        ActionType::ExecuteBlipFunction => context_store.blip_function += 1,
-        ActionType::ProcessContentAssistant => {},
-        ActionType::TrackEvent => context_store.trackings += 1,
-        ActionType::ForwardToAgent => context_store.agents += 1,
-        ActionType::ForwardToDesk => context_store.desk += 1,
-    }
-}
 
 fn get_router_children(tenant: &str, bot_id: &str) -> Vec<RouterChild> {
     let file = DataFile {
